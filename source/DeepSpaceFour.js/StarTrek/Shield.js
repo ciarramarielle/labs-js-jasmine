bdf0b6f81ccd8b34e39bc388d34d253e9f2a7481
Ciarra Marielle Altoveros <ciarraaltoveros@gmail.com>
2017-04-05 16:27:04 -0700

    energy transfers into shield

--- source/DeepSpaceFour.js/StarTrek/Shield.js
+++ source/DeepSpaceFour.js/StarTrek/Shield.js
@@ -1,8 +1,13 @@
 Shield = function() {
 	this.raised = false
+	this.energy = 0
 };
+
 Shield.prototype = {
 	raise: function() {
 		this.raised = true
+	},
+	transferEnergy : function(n) {
+		this.energy += n
 	}
 };
--- source/DeepSpaceFour.js/Tests/testing.test.js
+++ source/DeepSpaceFour.js/Tests/testing.test.js
@@ -7,4 +7,10 @@ describe("shields", () => {
 		shield.raise()
 		expect(shield.raised).toBeTruthy()
 	})
+
+	it("transfers n energy from ship reserves to shields", () => {
+		var current = shield.energy 
+		shield.transferEnergy(1)
+		expect(shield.energy).toEqual(current+1)
+	})
 })
