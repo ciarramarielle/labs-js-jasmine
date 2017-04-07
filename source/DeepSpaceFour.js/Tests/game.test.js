752763c42fee90d84331c34404286741fc496c13
Ciarra Marielle Altoveros <ciarraaltoveros@gmail.com>
2017-04-07 14:41:02 -0700

    added game stuff

---
+++ source/DeepSpaceFour.js/Tests/game.test.js
@@ -0,0 +1,12 @@
+var game
+describe("game", () => {
+    beforeEach(() => {
+        game = new Game()
+    })
+
+    it("should be defined", () => {
+        expect(game).toBeDefined()
+    })
+
+    
+})
