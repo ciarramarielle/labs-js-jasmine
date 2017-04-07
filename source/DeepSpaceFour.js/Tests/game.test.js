fb99339f01e8c6caa02532375d4d90a49fdfd70c
Ciarra Marielle Altoveros <ciarraaltoveros@gmail.com>
2017-04-07 15:21:38 -0700

    reorganize game spec

--- source/DeepSpaceFour.js/Tests/game.test.js
+++ source/DeepSpaceFour.js/Tests/game.test.js
@@ -4,29 +4,6 @@ describe("game", () => {
         game = new Game('novice')
     })
 
-    describe("should be initialized", () => {
-        it("should be defined", () => {
-            expect(game).toBeDefined()
-        })
-
-        it("should have #Klingons", () => {
-            expect(game.Klingons).toEqual(jasmine.any(Number))
-        })
-
-        it("should have #Starbases", () => {
-            expect(game.Starbases).toEqual(jasmine.any(Number))
-        })
-
-        it("should have #Stardates", () => {
-            expect(game.Stardates).toEqual(jasmine.any(Number))
-        })
-
-        it("should have my initial location", () => {
-            expect(game.location.x).toEqual(jasmine.any(Number))
-            expect(game.location.y).toEqual(jasmine.any(Number))
-        })
-    })
-
     describe("when getting a random number from bounds", () => {
         describe("when level: novice", () => {
             var retVal
@@ -65,6 +42,29 @@ describe("game", () => {
                 expect(retVal).not.toBeLessThan(0)
                 expect(retVal).not.toBeGreaterThan(game.levelBound)
             })
+        })
+    })
+    
+    describe("should be initialized", () => {
+        it("should be defined", () => {
+            expect(game).toBeDefined()
+        })
+
+        it("should have #Klingons", () => {
+            expect(game.Klingons).toEqual(jasmine.any(Number))
+        })
+
+        it("should have #Starbases", () => {
+            expect(game.Starbases).toEqual(jasmine.any(Number))
+        })
+
+        it("should have #Stardates", () => {
+            expect(game.Stardates).toEqual(jasmine.any(Number))
+        })
+
+        it("should have my initial location", () => {
+            expect(game.location.x).toEqual(jasmine.any(Number))
+            expect(game.location.y).toEqual(jasmine.any(Number))
         })
     })
 
