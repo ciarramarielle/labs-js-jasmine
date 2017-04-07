3fe026fef79171445dc286e75e4944f73856f780
Ciarra Marielle Altoveros <ciarraaltoveros@gmail.com>
2017-04-07 14:45:23 -0700

    d4 setup and tests done

--- source/DeepSpaceFour.js/StarTrek/Game.js
+++ source/DeepSpaceFour.js/StarTrek/Game.js
@@ -1,3 +1,9 @@
 Game = function() {
-    
+    this.Klingons = 1
+    this.Starbases = 1
+    this.Stardates = 1
+    this.location = {
+        x: 1,
+        y: 1
+    }
 }
--- source/DeepSpaceFour.js/Tests/game.test.js
+++ source/DeepSpaceFour.js/Tests/game.test.js
@@ -8,5 +8,21 @@ describe("game", () => {
         expect(game).toBeDefined()
     })
 
-    
+    it("should have #Klingons", () => {
+        expect(game.Klingons).toEqual(jasmine.any(Number))
+    })
+
+    it("should have #Starbases", () => {
+        expect(game.Starbases).toEqual(jasmine.any(Number))
+    })
+
+    it("should have #Stardates", () => {
+        expect(game.Stardates).toEqual(jasmine.any(Number))
+    })
+
+    it("should have my initial location", () => {
+        expect(game.location.x).toEqual(jasmine.any(Number))
+        expect(game.location.y).toEqual(jasmine.any(Number))
+    })
+
 })
