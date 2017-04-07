97f49f6f3417f934d85e80a8be933cf8c8515b3f
Ciarra Marielle Altoveros <ciarraaltoveros@gmail.com>
2017-04-07 15:10:08 -0700

    refactor level, initialize game by Game(level)

--- source/DeepSpaceFour.js/StarTrek/Game.js
+++ source/DeepSpaceFour.js/StarTrek/Game.js
@@ -1,13 +1,21 @@
-Game = function() {
+Game = function(level) {
+    switch (level) {
+        case 'novice':
+            this.levelBound = 5
+        case 'intermediate':
+            this.levelBound = 10
+        case 'expert':
+            this.levelBound = 15
+        default:
+            this.levelBound = 0
+    }
+
     this.Klingons = 1
     this.Starbases = 1
     this.Stardates = 1
     this.location = {
         x: 1,
         y: 1
-    }
-    this.level = {
-        novice: 5
     }
 }
 
--- source/DeepSpaceFour.js/Tests/game.test.js
+++ source/DeepSpaceFour.js/Tests/game.test.js
@@ -1,64 +1,73 @@
 var game
 describe("game", () => {
     beforeEach(() => {
-        game = new Game()
+        game = new Game('novice')
     })
 
-    it("should be defined", () => {
-        expect(game).toBeDefined()
-    })
+    describe("should be initialized", () => {
+        beforeEach(()=> {
+            // game.initialize()
+        })
 
-    it("should have #Klingons", () => {
-        expect(game.Klingons).toEqual(jasmine.any(Number))
-    })
+        it("should be defined", () => {
+            expect(game).toBeDefined()
+        })
 
-    it("should have #Starbases", () => {
-        expect(game.Starbases).toEqual(jasmine.any(Number))
-    })
+        it("should have #Klingons", () => {
+            expect(game.Klingons).toEqual(jasmine.any(Number))
+        })
 
-    it("should have #Stardates", () => {
-        expect(game.Stardates).toEqual(jasmine.any(Number))
-    })
+        it("should have #Starbases", () => {
+            expect(game.Starbases).toEqual(jasmine.any(Number))
+        })
 
-    it("should have my initial location", () => {
-        expect(game.location.x).toEqual(jasmine.any(Number))
-        expect(game.location.y).toEqual(jasmine.any(Number))
+        it("should have #Stardates", () => {
+            expect(game.Stardates).toEqual(jasmine.any(Number))
+        })
+
+        it("should have my initial location", () => {
+            expect(game.location.x).toEqual(jasmine.any(Number))
+            expect(game.location.y).toEqual(jasmine.any(Number))
+        })
     })
 
     describe("when getting a random number from bounds", () => {
         describe("when level: novice", () => {
             var retVal
             beforeEach(() => {
-                retVal = game.randomGenerate(0, game.level.novice)
+                game = new Game('novice')
+                retVal = game.randomGenerate(0, game.levelBound)
             })
 
             it("should return a number less than or equal to novice bound", () => {
-                expect(retVal).not.toBeLessThan(0) // >=0
-                expect(retVal).not.toBeGreaterThan(game.level.novice)
+                expect(retVal).not.toBeLessThan(0)                  // >=0
+                expect(retVal).not.toBeGreaterThan(game.levelBound) // <= level bound
             })
         })
         
         describe("when level: intermediate", () => {
             var retVal
             beforeEach(() => {
-                retVal = game.randomGenerate(0, game.level.intermediate)
+                game = new Game('intermediate')
+                retVal = game.randomGenerate(0, game.levelBound)
             })
 
             it("should return a number less than or equal to intermediate bound", () => {
                 expect(retVal).not.toBeLessThan(0)
-                expect(retVal).not.toBeGreaterThan(game.level.intermediate)
+                expect(retVal).not.toBeGreaterThan(game.levelBound)
             })
         })
 
         describe("when level: expert", () => {
             var retVal
             beforeEach(() => {
-                retVal = game.randomGenerate(0, game.level.expert)
+                game = new Game('expert')
+                retVal = game.randomGenerate(0, game.levelBound)
             })
 
             it("should return a number less than or equal to expert bound", () => {
                 expect(retVal).not.toBeLessThan(0)
-                expect(retVal).not.toBeGreaterThan(game.level.expert)
+                expect(retVal).not.toBeGreaterThan(game.levelBound)
             })
         })
     })
